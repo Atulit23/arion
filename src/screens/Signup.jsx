5ed08e449da41781846a3151d5fc87d3be801dbf
Atulit23 <atulit23@gmail.com>
2025-04-01 00:52:14 +0530

    new

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,72 +1,127 @@
-import React, { useState } from 'react'
-import styles from '../css/auth.module.css'
-import axios from 'axios'
-import { useDispatch, useSelector } from 'react-redux'
-import { storeUser } from '../redux/slices/appSlice'
+import React, { useState } from "react";
+import styles from "../css/auth.module.css";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { storeUser } from "../redux/slices/appSlice";
 import { toast, ToastContainer } from "react-toastify";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import logo from "../icons/logo_new.png";
+import user from '../icons/user.png';
+import email from '../icons/email.png';
+import lock from '../icons/lock.png';
 
 export default function Signup() {
-    const [fields, setFields] = useState({
-        username: "",
-        email: "",
-        password: ""
-    })
-    const dispatch = useDispatch()
-    const userData = useSelector((state) => state.app.userData);
-    const [error, setError] = useState(null)
-    const navigate = useNavigate()
-    console.log(userData)
-    console.log(error)
+  const [fields, setFields] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    otp: "",
+  });
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.app.userData);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  console.log(userData);
+  console.log(error);
 
-    const signup = async () => {
-        try {
-            const res = await axios.post("http://localhost:8000/auth/signup", fields);
-            console.log(res?.data?.newUser);
-            dispatch(storeUser(res?.data?.newUser));
-            navigate('/blogs')
-        } catch (err) {
-            console.error(err.response?.data);
-            console.log(err.message)
-            setError(err.response?.data)
-            let e = err.message
-            toast.error(err.response?.data?.message);
-        }
+  const signup = async () => {
+    try {
+      const res = await axios.post("http://localhost:8000/auth/signup", fields);
+      dispatch(storeUser(res?.data?.newUser));
+      navigate("/blogs");
+    } catch (err) {
+      setError(err.response?.data);
+      toast.error(err.response?.data?.message);
     }
+  };
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.main}>
-                <span className={styles.header}>arion</span>
-                <div className={styles.all}>
-                    <div className={styles.current}>
-                        {<span>Username</span>}
-                        <input type="text" placeholder='Username...' value={fields.username} className={styles.input} onChange={(e) => {
-                            setFields({ ...fields, username: e.target.value })
-                        }} />
-                    </div>
-                    <div className={styles.current}>
-                        {<span>Email</span>}
-                        <input type="text" placeholder='Email...' value={fields.email} className={styles.input} onChange={(e) => {
-                            setFields({ ...fields, email: e.target.value })
-                        }} />
-                    </div>
-                    <div className={styles.current}>
-                        {<span>Password</span>}
-                        <input type="text" placeholder='Password...' value={fields.password} className={styles.input} onChange={(e) => {
-                            setFields({ ...fields, password: e.target.value })
-                        }} />
-                    </div>
-                    <div className={styles.btn} onClick={() => {
-                        signup()
-                    }}>
-                        Sign up
-                    </div>
-                    <div className={styles.already}>
-                        Already have an account? Login
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <div className={styles.logo__section}>
+          <img src={logo} alt="logo" className={styles.logo} />
+          <span className={styles.header}>arion</span>
         </div>
-    )
+        {/* <span className={styles.info}>Please enter a unique username and a valid email.</span> */}
+        <div className={styles.all}>
+          <div className={styles.current}>
+            {/* {<span>Username</span>} */}
+            <img src={user} alt="" className={styles.user} />
+            <input
+              type="text"
+              placeholder="Username"
+              value={fields.username}
+              className={styles.input}
+              onChange={(e) => {
+                setFields({ ...fields, username: e.target.value });
+              }}
+            />
+          </div>
+          <div className={styles.current}>
+            {/* {<span>Email</span>} */}
+            <img src={email} alt="" className={styles.user} />
+            <input
+              type="text"
+              placeholder="Email"
+              value={fields.email}
+              className={styles.input}
+              onChange={(e) => {
+                setFields({ ...fields, email: e.target.value });
+              }}
+            />
+          </div>
+          <div className={styles.current}>
+            {/* {<span>Password</span>} */}
+            <img src={lock} alt="" className={styles.user} />
+            <input
+              type="password"
+              placeholder="Password"
+              value={fields.password}
+              className={styles.input}
+              onChange={(e) => {
+                setFields({ ...fields, password: e.target.value });
+              }}
+            />
+          </div>
+          <div className={styles.current}>
+            {/* {<span>Password</span>} */}
+            <img src={lock} alt="" className={styles.user} />
+            <input
+              type="text"
+              placeholder="Confirm Password"
+              value={fields.confirmPassword}
+              className={styles.input}
+              onChange={(e) => {
+                setFields({ ...fields, confirmPassword: e.target.value });
+              }}
+            />
+          </div>
+          {/* <div className={styles.current}>
+            <input
+              type="text"
+              placeholder="Confirm Password"
+              value={fields.confirmPassword}
+              className={styles.input}
+              onChange={(e) => {
+                setFields({ ...fields, confirmPassword: e.target.value });
+              }}
+            />
+          </div> */}
+          <div
+            className={styles.btn}
+            onClick={() => {
+              signup();
+            }}
+          >
+            Sign up
+          </div>
+          <div className={styles.already}>
+            <span>Forgot password?</span>
+            <span>Login</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
